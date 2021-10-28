53e73bb0ac2c5ace6754d19d9c28fedf1a847589
Joe Stanley <engineerjoe440@yahoo.com>
2021-10-27 19:31:55 -0700

    cleaned up python basics

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,21 @@
 ################################################################################
 """
-DJ JOE Website Availability Calendar
-------------------------------------
+4-H Photo Upload Service
+------------------------
 
 (c) 2021 - Stanley Solutions - Joe Stanley
 
-This application serves the React frontend required to demonstrate the available
-dates for DJ Joe Services.
+This application serves to provide backend services for a React.js photo upload
+system that can be used by volunteers and youth to engage with event photos.
 """
 ################################################################################
 
 # Requirements
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
-from datetime import datetime
-from date_support import weekends_in_range, remove_excluded_dates
-from google_calendar import get_occupied_dates
 
-
-class DatePeriod(BaseModel):
-    start: datetime
-    end: datetime
 
 # Application Base
 app = FastAPI()
@@ -32,28 +24,6 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# Available Date List Generator
-def generate_available_dates(dates: DatePeriod):
-    print(dates.start, dates.end)
-    # Identify Available Date Information
-    included_dates = weekends_in_range(start=dates.start, end=dates.end)
-    excluded_dates = get_occupied_dates(start=dates.start, end=dates.end)
-    # Remove Days That are Busy
-    days_avail = remove_excluded_dates(
-        inclusive_dates=included_dates,
-        exclude_dates=excluded_dates,
-    )
-    available = []
-    # Iteratively Build Structure for JSON
-    for date in days_avail:
-        available.append({
-            'title': "Available!",
-            'start': date,
-            'end': date,
-            'allDay': True,
-            'resource': '',
-        })
-    return available
 
 # Main Application Response
 @app.get("/", response_class=HTMLResponse)
@@ -67,6 +37,6 @@ async def root(request: Request):
     )
 
 # Main API Endpoint to Serve Available Dates
-@app.post("/api/getavailability")
-async def availability(dates: DatePeriod):
-    return {"events": generate_available_dates(dates=dates)}
+@app.post("/api/upload")
+async def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
